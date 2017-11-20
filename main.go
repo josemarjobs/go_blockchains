db5f17a7d59d077526292e26bf0a3937c1718de8
Josemar Magalhaes <josemarmagalhaes@Josemars-MacBook-Pro.local>
2017-11-19 23:56:43 -0800

    Simple Blockchain implementation, Initial Commit

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/josemarjobs/blockchain/block"
+)
+
+func main() {
+
+	bc := block.NewBlockChain()
+
+	bc.AddBlock("Send 1 BTC to Peter Griffin")
+	bc.AddBlock("Send more 2 BTC to Peter Griffin")
+
+	for _, block := range bc.Blocks {
+		fmt.Printf("Prev Hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n\n", block.Hash)
+	}
+}
